f0d3a377a45070e2b393a8126331dc221b1b97bf
matheusantonio156 <164435308+matheusantonio156@users.noreply.github.com>
2024-05-20 15:44:38 -0300

    main.jsx

--- React-main/name-of-your-project/src/main.jsx
+++ React-main/name-of-your-project/src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
-nt.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
